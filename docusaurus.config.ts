ed539504910baa2d839709ddd8cb8f5d2481f20a
abdul <cseabdulrahim@gmail.com>
2025-09-10 16:26:39 +0530

    fix: update repository name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,13 +19,13 @@ const config: Config = {
   url: 'https://abdulrahim2002.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/open-pocket-backend-server/',
+  baseUrl: '/open-pocket-docs/',
   
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'abdulrahim2002', // Usually your GitHub org/user name.
-  projectName: 'open-pocket-backend-server', // Usually your repo name.
+  projectName: 'open-pocket-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
